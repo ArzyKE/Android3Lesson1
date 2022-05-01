c72c883b9b055e31918ebea27cf7eee626b191b5
ArzyKE <arzymatkadyrov968@gmail.com>
2022-05-01 15:42:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android3lesson1/ui/activity/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.android3lesson1.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.android3lesson1.databinding.ActivityMainBinding;
+import com.example.android3lesson1.models.NameModel;
+import com.example.android3lesson1.ui.adapter.ListAdapter;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MainViewModel viewModel;
+
+    ListAdapter listAdapter = new ListAdapter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initialization();
+        setupListener();
+    }
+
+    private void initialization() {
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding.list.setAdapter(listAdapter);
+    }
+
+    private void addObserver() {
+        viewModel.mutableLiveData.observe(this, new Observer<List<NameModel>>() {
+            @Override
+            public void onChanged(List<NameModel> nameModels) {
+                listAdapter.setData(nameModels);
+            }
+        });
+    }
+
+    private void setupListener() {
+        binding.btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addObserver();
+                viewModel.getList();
+                binding.btnDisplay.setVisibility(View.GONE);
+            }
+        });
+    }
+}
